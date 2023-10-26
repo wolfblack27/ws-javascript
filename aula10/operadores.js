d060733adb66a093f316063182dd31cb242d9b05
wolfblack27 <tigowolfblack27@gmail.com>
2023-10-26 15:55:12 -0300

    exemplos usando o windows

--- aula10/operadores.js
+++ aula10/operadores.js
@@ -1,27 +1,27 @@
+/**
+ * Operadores Aritméticos:
+ *  +       => adição e concatenação
+ *  -       => subtrai
+ *  **      =>potenciação
+ *  %       =>mod, guarda o resto da divisao
+ *  ++      => operador de incremento
+ *
+ * Conversão explicita:
+ *    Numberpaser
+ *    parseInt
+ *    parseFloat
+ */
 
-  /**
-   * Operadores Aritméticos:
-   *  +       => adição e concatenação
-   *  -       => subtrai
-   *  **      =>potenciação
-   *  %       =>mod, guarda o resto da divisao
-   *  ++      => operador de incremento  
-   *
-   */
+const num1 = 10;
+const num2 = 5;
+let contador = 0;
+contador++; //soma 1
+contador++; //soma 1
+contador += num1; // soma o valor que já esta em contador e soma 10
+let numberString = "5";
 
-  const num1 = 10;
-  const num2 = 5;
-  let contador=0;
-  contador++ //soma 1
-  contador++ //soma 1
-  contador+=num1 // soma o valor que já esta em contador e soma 10
-  let numberString = '5'
-  
-
-  console.log(` Valor da soma: ${num1 + num2}`);
-  console.log(` Valor da subtração: ${num1 - num2}`);
-  console.log(` Valor da potencia: ${num1 ** num2}`);
-  console.log(` Valor do resto: ${num1 % num2}`);
-  console.log(` Valor de contador incrementado: ${contador}`)      
-  
-
+console.log(` Valor da soma: ${num1 + num2}`);
+console.log(` Valor da subtração: ${num1 - num2}`);
+console.log(` Valor da potencia: ${num1 ** num2}`);
+console.log(` Valor do resto: ${num1 % num2}`);
+console.log(` Valor de contador incrementado: ${contador}`);
---
+++ aula11/js/funcNavegador.js
@@ -0,0 +1,10 @@
+alert('Mensagem') //Exibe uma mensagem na tela
+let flag =confirm("Duvida")//Exibi uma mensagem do tipo
+let dado = prompt("Digite qualquer coisa")
+
+if (flag){
+alert("Resposta foi sim")
+
+} 
+
+console.log("Voce digitou: " +dado)
