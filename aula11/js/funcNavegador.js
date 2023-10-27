3a2f936ed9982673abd2ba2b3c85d909c358e1ed
wolfblack27 <tigowolfblack27@gmail.com>
2023-10-26 21:38:06 -0300

    aula 11

--- aula11/js/funcNavegador.js
+++ aula11/js/funcNavegador.js
@@ -1,10 +1,9 @@
-alert('Mensagem') //Exibe uma mensagem na tela
-let flag =confirm("Duvida")//Exibi uma mensagem do tipo
-let dado = prompt("Digite qualquer coisa")
+alert("Mensagem"); //Exibe uma mensagem na tela
+let flag = confirm("Duvida"); //Exibi uma mensagem do tipo
+let dado = prompt("Digite qualquer coisa");
 
-if (flag){
-alert("Resposta foi sim")
+if (flag) {
+  alert("Resposta foi sim");
+}
 
-} 
-
-console.log("Voce digitou: " +dado)
+console.log("Voce digitou: " + dado);
