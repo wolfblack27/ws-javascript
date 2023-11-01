59c62f6066be4ca963adcf17e5352809749727d9
wolfblack27 <tigowolfblack27@gmail.com>
2023-11-01 15:37:35 -0300

    ressolucao

---
+++ aula13/methodsStrings.js
@@ -0,0 +1,6 @@
+let umaString = "Um \"texto\""; // escapando um caracter
+
+console.log(umaString +'\n')
+novastring = umaString.replace('Um','O')
+console.log('Nova String \n')
+console.log(novastring)
